23ef7bad05985977e85666eee611cfd7550e4e91
baraline <antoine.guillaume@etu.univ-orleans.fr>
2015-10-16 00:25:41 +0200

    Modif value int en cours

--- value_int.c
+++ value_int.c
@@ -9,10 +9,6 @@
 
 # undef NDEBUG   // FORCE ASSERT ACTIVATION
 
-
-
-
-
 /*!
  * \file 
  * \brief \c value to hold an \c int.
@@ -29,8 +25,41 @@
  * \copyright GNU Public License.
  */
 
-
+// FAIRE DECLARATION DANS .H
 VALUE_DECLARE( int , long long int ) 
 
+void print();
+void destroy();
+chunk copy();
 
+boolean chunk_is_int(chunk ToTest){ 
+  if(chunk_is_value(ToTest)){ // Test acces a la méthode
+    return value_is_int(ToTest);
+  }else{
+    return false;
+  }
+}
 
+boolean value_is_int(chunk ToTest){
+  if(isdigit(ToTest->state->value) != 0){ // tester acces champs value sur basic_type
+    return true;
+  }else{
+    return false;
+  }
+}
+
+basic_type value_int_get_value(chunk ToGet){
+  if(ToGet->state != NULL){
+    return ToGet->state;
+  }else{
+    basic_type res = basic_type_error; // a tester
+    return res;
+  }
+}
+
+chunk value_int_create(basic_type Type_c){
+  chunk res = malloc(sizeof(struct chunk_struct));
+  res->message_action = MESSAGE_ACTION__BASIC_VALUE(int); // Verif acces au #define de value
+  res->state = NULL; // Verif initialisation de la valeur
+  return res;
+}
