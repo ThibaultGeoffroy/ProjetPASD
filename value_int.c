e4678df22525c222b509a23d2a65d678f2d8d49b
baraline <antoine.guillaume@etu.univ-orleans.fr>
2015-10-17 21:47:23 +0200

    Premiere implementation, teste a effectuer

--- value_int.c
+++ value_int.c
@@ -3,7 +3,7 @@
 # include <assert.h>
 
 # include "value_int.h"
-
+# include "stdbool.h"
 # include "macro_value_c.h"
 
 
@@ -28,29 +28,50 @@
 // FAIRE DECLARATION DANS .H
 VALUE_DECLARE( int , long long int ) 
 
-void print();
-void destroy();
-chunk copy();
+void print(chunk toprint, FILE *f){ // Que doit on print ? state / methode associer au message ... ?
 
-boolean chunk_is_int(chunk ToTest){ 
+}
+
+void destroy(chunk *todestroy){ // fonction a tester, voir destruction necessaire pour message action et state, verif interférence avec copy)
+  /* param const comment le free , besoins de le free ?
+  free((*todestroy)->reactions);
+  */
+(*todestroy)->reactions = NULL;
+  free((*todestroy)->state);
+  (*todestroy)->state = NULL;
+  free((*todestroy));
+  *todestroy = NULL;
+  free(&todestroy);
+  todestroy = NULL;
+  return ;
+}
+
+chunk copy(chunk origin){
+  chunk res = malloc(sizeof(struct chunk_struct));
+  res->reactions = origin->reactions;
+  res->state = origin->state;
+  return res;
+}
+
+bool chunk_is_int(chunk ToTest){ 
   if(chunk_is_value(ToTest)){ // Test acces a la méthode
     return value_is_int(ToTest);
   }else{
     return false;
   }
 }
-
-boolean value_is_int(chunk ToTest){
+/* Utilisation de la fonction dans macro_value_c.h, test si fonctionne correctement 
+bool value_is_int(chunk ToTest){
   if(isdigit(ToTest->state->value) != 0){ // tester acces champs value sur basic_type
     return true;
   }else{
     return false;
   }
 }
-
+*/
 basic_type value_int_get_value(chunk ToGet){
   if(ToGet->state != NULL){
-    return ToGet->state;
+    return *(basic_type*)ToGet->state;
   }else{
     basic_type res = basic_type_error; // a tester
     return res;
@@ -59,7 +80,7 @@ basic_type value_int_get_value(chunk ToGet){
 
 chunk value_int_create(basic_type Type_c){
   chunk res = malloc(sizeof(struct chunk_struct));
-  res->message_action = MESSAGE_ACTION__BASIC_VALUE(int); // Verif acces au #define de value
+  res->reactions = MESSAGE_ACTION__BASIC_VALUE(int); // Verif acces au #define de value
   res->state = NULL; // Verif initialisation de la valeur
   return res;
 }
